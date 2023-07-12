52067eaa668de27870ce5881fe17b104898da840
Luara Amaral <adsluara@gmail.com>
2023-07-12 17:33:37 -0300

    Adicionando validações com Bean Validation

--- src/main/java/med/voll/api/controller/Controller.java
+++ src/main/java/med/voll/api/controller/Controller.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -18,7 +19,7 @@ public class Controller {
 
     @PostMapping(value = "/cadastraMedico")
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         medico.save(new Medico(dados));
     }
 
