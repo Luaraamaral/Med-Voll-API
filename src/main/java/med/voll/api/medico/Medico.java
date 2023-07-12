8db129250f256c35326617a0621c2bc867669143
Luara Amaral <adsluara@gmail.com>
2023-07-12 17:42:45 -0300

    Criação do atributo telefone na entidade e record Medico, e criação da segunda migration

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -13,16 +13,17 @@ public class Medico {
     private Integer id;
     private String nome;
     private String email;
+    private String telefone;
     private String crm;
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
     @Embedded
     private Endereco endereco;
 
-
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
+        this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
